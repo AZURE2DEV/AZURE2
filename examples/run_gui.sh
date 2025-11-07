25a1f1489d346e3397caae2725cdcfb0b68d780c
Akhil Bhardwaj <abhard5@lsu.edu>
2025-11-07 16:32:55 -0600

    changed dockerfile and rungui file to run on macos

--- examples/run_gui.sh
+++ examples/run_gui.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+export XAUTH=$HOME/.Xauthority
+
 docker run -it \
        --privileged \
        --hostname luna \
-       -e "DISPLAY=$DISPLAY" \
+       -e DISPLAY=host.docker.internal:0 \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
-       -v "$XAUTH/.Xauthority:/root/.Xauthority" \
+       -v "$XAUTH:/root/.Xauthority" \
        -v "$PWD/azure2:/workspace/azure2" \
        azure2:latest bash -c 'cd /workspace/azure2 && \
-       	      	                source /workspace/app/install/root/bin/thisroot.sh && \
-				export QT_GRAPHICSSYSTEM="native" && \
-       	      	       	        AZURE2'
+                                source /workspace/app/install/root/bin/thisroot.sh && \
+                                export QT_GRAPHICSSYSTEM="native" && \
+                                AZURE2'
